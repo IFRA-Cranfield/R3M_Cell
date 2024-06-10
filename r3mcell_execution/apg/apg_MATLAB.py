99650f32a5e9e8b40e5e397f30c80307fda2125f
MikelBueno <Mikel.Bueno-Viso@cranfield.ac.uk>
2024-06-10 16:52:44 +0100

    Upload (and modify): APG model for CubeStacking.

--- r3mcell_execution/apg/apg_MATLAB.py
+++ r3mcell_execution/apg/apg_MATLAB.py
@@ -27,17 +27,8 @@ from r3mcell_data.srv import SkillExecution
 import numpy as np
 import matlab.engine
 
-# IMPORT -> getRecipe.py:
-from getRecipe import computeRecipe
-
-# ================ #
-# GLOBAL VARIABLES #
-RobPose = Robpose()
-GripperState = 0
-
 # ========================================================================================= #
 # Execute Skill -> ROS 2 Service Client:
-
 class R3MSkillClient(Node):
 
     def __init__(self):
@@ -56,44 +47,47 @@ class R3MSkillClient(Node):
         self.req_SKILL.id = ID
         self.future_SKILL = self.cli_SKILL.call_async(self.req_SKILL)
 
-# ========================================================================================= #
-# Get RobotPose:
-class RobotPoseSubscriber(Node):
+# FUNCTION -> EXECUTE SKILL:
+def ExecuteSkill(CLIENT, ID):
 
-    def __init__(self):
+    CLIENT.SKILL_REQUEST(ID)
+    while rclpy.ok():
+        rclpy.spin_once(CLIENT)
+        if CLIENT.future_SKILL.done():
+            try:
+                skillRES = CLIENT.future_SKILL.result().result
+            except Exception as exc:
+                print("[R3M Cell] - /ExecuteSkill ROS2 Service call failed. ERROR: " + str(exc))
+                return(False)
+            else:
+                print("[R3M Cell] - /ExecuteSkill RESULT:")
+                print(skillRES)
+                print("")
+            break
 
-        super().__init__("r3mcell_APGMatlab_RobotPoseSubscriber")
-        self.subscription_ = self.create_subscription(Robpose, "/Robpose", self.CALLBACK, 1)
-
-    def CALLBACK(self, POSE):
-
-        global RobPose
-        RobPose = POSE
-
-        RobPose.x = round(RobPose.x, 2)
-        RobPose.y = round(RobPose.y, 2)
-        RobPose.z = round(RobPose.z, 2)
-        RobPose.qx = round(RobPose.qx, 2)
-        RobPose.qy = round(RobPose.qy, 2)
-        RobPose.qz = round(RobPose.qz, 2)
-        RobPose.qw = round(RobPose.qw, 2)
+    return(skillRES)
         
 # ========================================================================================= #
 # MatlabAgent:
 class MatlabAgent():
     
-    def __init__(self):
+    def __init__(self, UseCase):
 
         super().__init__()
         self.MATLAB = matlab.engine.start_matlab()
         
-        self.PATH = os.path.join(get_package_share_directory('r3mcell_execution'), 'r3m_APG')
+        self.PATH = os.path.join(get_package_share_directory('r3mcell_execution'), 'apg', 'agents', UseCase)
         self.AGENT = "agentData.mat"
         
-    def Execute(self, id, RobPose, EEState, ObjectPose):
+    def Execute(self, ID, RobState, EEState, ObjState, ObjectNO):
         
         # ASSIGN -> Input values to AGENT:
-        observation = [id, RobPose.x, RobPose.y, RobPose.z, RobPose.qx, RobPose.qy, RobPose.qz, RobPose.qw, EEState, ObjectPose.x, ObjectPose.y, ObjectPose.z, ObjectPose.qx, ObjectPose.qy, ObjectPose.qz, ObjectPose.qw]
+
+        if ObjectNO == 1:
+            observation = [ID, RobState, EEState, ObjState[0]]
+        elif ObjectNO == 2:
+            observation = [ID, RobState, EEState, ObjState[0], ObjState[1]]
+        
         observation = np.array(observation)
         
         self.MATLAB.addpath(self.PATH)
@@ -107,76 +101,53 @@ class MatlabAgent():
 
 def main(args=None):
     
-    global RobPose
-    
     rclpy.init(args=args)
     
     # PRINT:
     print("==========================================================")
     print("R3M - AUTOMATIC PROGRAM GENERATION: Execution of APG Agent")
     print("")
-    
-    # 0. COMPUTE RECIPE FROM OBJECT POSE!
-    #print("Computing RECIPE parameters from OBJECT POSITION...")
-    #EditRecipe = computeRecipe("box")
-    #EditRecipe.smallCUBE()
-    #print("DONE")
-    #print("")
+
+    # INPUT PARAMETERS:
+    ObjectNO = 2
+    UseCase = "CubeStacking"
 
     # INITIALISE ROS 2 Classes:
     Node_SkillExecution = R3MSkillClient()
-    Node_RobotPose = RobotPoseSubscriber()
-    APG_Agent = MatlabAgent()
-    
-    print("Initialising RLA: Executing Recipe N1...")
+    APG_Agent = MatlabAgent(UseCase)
     
     # INITIAL STATE -> Recipe N1 will be executed as a initial step, to get the state of the system:
-    Node_SkillExecution.SKILL_REQUEST(1)
-    while rclpy.ok():
-        rclpy.spin_once(Node_SkillExecution)
-        if Node_SkillExecution.future_SKILL.done():
-            try:
-                skillRES = Node_SkillExecution.future_SKILL.result().result
-            except Exception as exc:
-                print("[R3M Cell] - /ExecuteSkill ROS2 Service call failed. ERROR: " + str(exc))
-                return(False)
-            else:
-                print("[R3M Cell] - /ExecuteSkill RESULT:")
-                print(skillRES)
-                print("")
-            break
-        
+    print("Initialising RLA: Executing Recipe N1...")
+    skillRESULT = ExecuteSkill(Node_SkillExecution, 1)
+    
     ID = 0
     
-    T = time.time() + 0.5
-    while (time.time() < T):
-        rclpy.spin_once(Node_RobotPose) # We assign current pose to RobPose.
+    RobState = skillRESULT.robstate.step
+    EEState = skillRESULT.robstate.endeffector
     
-    ObjPose = skillRES.product[0].currentpose
-    ObjPose.x = round(ObjPose.x, 2)
-    ObjPose.y = round(ObjPose.y, 2)
-    ObjPose.z = round(ObjPose.z, 2)
-    ObjPose.qx = round(ObjPose.qx, 2)
-    ObjPose.qy = round(ObjPose.qy, 2)
-    ObjPose.qz = round(ObjPose.qz, 2)
-    ObjPose.qw = round(ObjPose.qw, 2)
-    
-    EEState = skillRES.endeffector
+    ObjState = []
+    for i in range(ObjectNO):
+        ObjState.append(skillRESULT.product[i].step)
     
     CONTINUE = True
     while CONTINUE:
         
         print("RECIPE EXECUTION: Getting Recipe ID from RLA...")
-        print("Robot Pose:")
-        print(RobPose)
-        print("Object Pose:")
-        print(ObjPose)
-        print("Gripper State:")
+        
+        print("Robot State:")
+        print(RobState)
+        
+        print("End Effector State:")
         print(EEState)
+
+        print("Object State:")
+        for i in range(ObjectNO):
+            print("- Object N" + str(i+1) + "" + skillRESULT.product[i].name + " -> " + str(ObjState[i]))
+
         print("")
         
         # GET -> RECIPE ID from AGENT:
-        RECIPE = APG_Agent.Execute(ID, RobPose, EEState, ObjPose)
+        RECIPE = APG_Agent.Execute(ID, RobState, EEState, ObjState, ObjectNO)
         RECIPE_ID = int(RECIPE)
         
         print("RECIPE ID obtained! ")
@@ -186,36 +157,19 @@ def main(args=None):
         print("Executing Recipe N" + str(RECIPE_ID) + "...")
         
         # EXECUTE -> RECIPE:
-        Node_SkillExecution.SKILL_REQUEST(RECIPE_ID)
-        while rclpy.ok():
-            rclpy.spin_once(Node_SkillExecution)
-            if Node_SkillExecution.future_SKILL.done():
-                try:
-                    skillRES = Node_SkillExecution.future_SKILL.result().result
-                except Exception as exc:
-                    print("[R3M Cell] - /ExecuteSkill ROS2 Service call failed. ERROR: " + str(exc))
-                    return(False)
-                else:
-                    print("[R3M Cell] - /ExecuteSkill RESULT:")
-                    print(skillRES)
-                    print("")
-                break
+        skillRESULT = ExecuteSkill(Node_SkillExecution, RECIPE_ID)
             
-        ID = float(skillRES.id)
-        T = time.time() + 0.5
-        while (time.time() < T):
-            rclpy.spin_once(Node_RobotPose) # We assign current pose to RobPose.
+        ID = float(skillRESULT.id)
+
+        # Check if -> LIAISON MET + ID=1, then FINISH!
+        # TBD.
         
-        ObjPose = skillRES.product[0].currentpose
-        ObjPose.x = round(ObjPose.x, 2)
-        ObjPose.y = round(ObjPose.y, 2)
-        ObjPose.z = round(ObjPose.z, 2)
-        ObjPose.qx = round(ObjPose.qx, 2)
-        ObjPose.qy = round(ObjPose.qy, 2)
-        ObjPose.qz = round(ObjPose.qz, 2)
-        ObjPose.qw = round(ObjPose.qw, 2)
+        RobState = skillRESULT.robstate.step
+        EEState = skillRESULT.robstate.endeffector
         
-        EEState = skillRES.endeffector
+        ObjState = []
+        for i in range(ObjectNO):
+            ObjState.append(skillRESULT.product[i].step)
 
     # FINISH:
     rclpy.shutdown()
